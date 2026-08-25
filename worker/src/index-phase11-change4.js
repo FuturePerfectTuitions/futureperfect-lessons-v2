6b146964dcc38ad684da12d3c738084f15509057
FuturePerfectTuitions <futureperfectlessons@gmail.com>
2026-08-25 12:44:18 +0100

    Route all shared Maths Year Answer Packs to view-specific PDFs

--- worker/src/index-phase11-change4.js
+++ worker/src/index-phase11-change4.js
@@ -1,9 +1,9 @@
 import phase11EfficientWorker from './index-phase11-efficient.js';
-import { prepareYear5AnswerPdfEnv } from './phase11-year5-answer-pdf.js';
+import { prepareSharedMathsAnswerPdfEnv } from './phase11-shared-maths-answer-pdf.js';
 
 export default {
   async fetch(request, env, ctx) {
-    const runtimeEnv = await prepareYear5AnswerPdfEnv(request, env);
+    const runtimeEnv = await prepareSharedMathsAnswerPdfEnv(request, env);
     return phase11EfficientWorker.fetch(request, runtimeEnv, ctx);
   }
 };
