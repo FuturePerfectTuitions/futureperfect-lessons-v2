9b3f5d26f752d6475fe1205951f71d164b11105b
FuturePerfectTuitions <futureperfectlessons@gmail.com>
2026-09-06 19:32:06 +0100

    Load first-name portal personalisation

--- config.js
+++ config.js
@@ -3,6 +3,8 @@ window.FPT_V2_CONFIG = Object.freeze({
   workerBaseUrl: "https://fpt-portal-v2-worker.futureperfectlessons.workers.dev"
 });
 
+document.write('<script src="assets/personalise-portal.js"></script>');
+
 if (/\/phase11\.html$/.test(window.location.pathname)) {
   document.write('<script src="assets/phase11-vr-howto.js"></script>');
 }
