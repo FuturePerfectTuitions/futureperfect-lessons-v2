032910c35e3a9a7c22d79d4ac8a787913132ef3e
FuturePerfectTuitions <futureperfectlessons@gmail.com>
2026-09-08 19:06:45 +0100

    Lock actual embedded signature hash

--- worker/src/parent-email-signature-clean.js
+++ worker/src/parent-email-signature-clean.js
@@ -5,4 +5,4 @@ import part4 from './parent-email-signature-clean-part4.js';
 
 export const FPT_EMAIL_SIGNATURE_CLEAN_BASE64 = `${part1}${part2}${part3}${part4}`;
 export const FPT_EMAIL_SIGNATURE_BYTES = 10570;
-export const FPT_EMAIL_SIGNATURE_SHA256 = '8444f6ba28a4ba94f7e815a2c5917c95999083454cec0407aa7aaf19f507470d';
+export const FPT_EMAIL_SIGNATURE_SHA256 = 'cc398aa099fb8aaaafdc7c3e7c08309e066ab33d8729907ee8606c3de5ebed4a';
