f9e2fc7f24fa5b291770a147783f25c56d3bc972
FuturePerfectTuitions <futureperfectlessons@gmail.com>
2026-09-08 19:04:50 +0100

    Correct embedded signature byte count

--- worker/src/parent-email-signature-clean.js
+++ worker/src/parent-email-signature-clean.js
@@ -4,5 +4,5 @@ import part3 from './parent-email-signature-clean-part3.js';
 import part4 from './parent-email-signature-clean-part4.js';
 
 export const FPT_EMAIL_SIGNATURE_CLEAN_BASE64 = `${part1}${part2}${part3}${part4}`;
-export const FPT_EMAIL_SIGNATURE_BYTES = 10606;
+export const FPT_EMAIL_SIGNATURE_BYTES = 10570;
 export const FPT_EMAIL_SIGNATURE_SHA256 = '8444f6ba28a4ba94f7e815a2c5917c95999083454cec0407aa7aaf19f507470d';
